b8bfead635b79db8548f02931044e1ad090ebc0c
Aya Idrissi <aya.zoujari.id@gmail.com>
2024-12-28 18:52:32 +0100

    28-12

---
+++ untitled/src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        // To print a text we can use System.out.println or System.out.print
+        System.out.println("Hello, World!");
+        System.out.print("Hii\n");
+
+        int x ; // declaration
+        x = 12; // assignement
+        int y = 3; // initialization
+
+        System.out.println(x);
+        System.out.println(x + y);
+
+        System.out.println("x = " +x);
+        System.out.println("x = " +x+ "\t y = " +y);
+
+        char c = '@';
+        boolean b=true;
+        double d = 3.14;
+        float f = 2.1f;
+        System.out.println(c + " \t" + b + " \t" + d + " \t" + f);
+
+        // For stringss we should write it with capital letter S because it's a reference, every ref start with capital letter
+
+        String s = "Aya";
+        System.out.println(s);
+
+        // Swap file
+        Swap.swap();
+
+        UserInput.userInput();
+
+    }
+}
+
---
+++ untitled/src/Swap.java
@@ -0,0 +1,12 @@
+public class Swap {
+    public static void swap(){
+        int a = 3;
+        int b = 2;
+        int c = 0;
+        System.out.println(" Befor swaping : \n "+"a = " +a+ "\nb = " +b+ "\nc = " +c );
+        c = a;
+        a = b;
+        b = c;
+        System.out.println(" After swaping : \n"+"a = " +a+ "\nb = " +b+ "\nc = " +c );
+    }
+}
